09356a71572850a90aaaa164964efe763b595e9e
SanketTops <sanket.tops@gmail.com>
2024-03-09 16:23:55 +0530

    Loops

---
+++ Module-1/alpha_pattern.py
@@ -0,0 +1,8 @@
+"""for i in range(65,91):
+    print(chr(i))"""
+
+c=65
+for i in range(1,6):
+    for j in range(i):
+        print(chr(c+j),end=" ")
+    print(" ")
---
+++ Module-1/while.py
@@ -0,0 +1,5 @@
+i=2
+
+while i<=10: #true
+    print(i)
+    i+=2
