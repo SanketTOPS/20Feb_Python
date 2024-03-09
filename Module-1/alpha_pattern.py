d6ce68be63ef92a89ff11b4e9fafc46c23b12a36
SanketTops <sanket.tops@gmail.com>
2024-03-09 16:28:04 +0530

    Loop - Patterns

--- Module-1/alpha_pattern.py
+++ Module-1/alpha_pattern.py
@@ -1,8 +1,14 @@
 """for i in range(65,91):
     print(chr(i))"""
 
-c=65
+"""c=65
 for i in range(1,6):
     for j in range(i):
         print(chr(c+j),end=" ")
+    print(" ")"""
+
+
+for i in range(5):
+    for j in range(i+1):
+        print(chr(j+65),end=" ")
     print(" ")
