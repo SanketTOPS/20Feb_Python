49cfd77fb97fd570e861858926f8bf86c08fb1b7
SanketTops <sanket.tops@gmail.com>
2024-03-12 16:02:53 +0530

    Strings

---
+++ Module-1/string.py
@@ -0,0 +1,33 @@
+mystr="this is python!"
+"""print(mystr)
+print(mystr[0])
+print(mystr[-1])
+print(mystr[0:4]) #string slice (range)
+print(mystr[5:])
+print(mystr[:4])"""
+
+# -------------------- String Method ------------------- #
+
+print(len(mystr))
+print(mystr)
+#print(mystr.strip())
+#print(mystr.lower())
+#print(mystr.upper())
+#print(mystr.replace('T','Y'))
+#print(mystr.replace('This','That'))
+#print(mystr.capitalize())
+#print(mystr.casefold())
+#print(mystr.count('i'))
+#print(mystr.count('Python'))
+#print(mystr.endswith("!"))
+#print(mystr.startswith("This"))
+#print(mystr.find('Python'))
+#print(mystr.index('is'))
+#print(mystr.isalpha())
+#print(mystr.isdigit())
+#print(mystr.isalnum())
+#print(mystr.islower())
+#print(mystr.isupper())
+print(mystr.title())
+
+
