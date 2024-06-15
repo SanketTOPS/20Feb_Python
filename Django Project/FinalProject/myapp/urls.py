9d19e9e55393e388bec067b65dd3d0a67320c0c5
SanketTops <sanket.tops@gmail.com>
2024-06-15 16:17:09 +0530

    Auth

--- Django Project/FinalProject/myapp/urls.py
+++ Django Project/FinalProject/myapp/urls.py
@@ -4,7 +4,7 @@ from myapp import views
 
 urlpatterns = [
     path('',views.index),
-    path('login/',views.login),
+    path('login/',views.login,name='login'),
     path('signup/',views.signup),
     path('notes/',views.notes),
     path('about/',views.about),
--- Django Project/FinalProject/myapp/views.py
+++ Django Project/FinalProject/myapp/views.py
@@ -1,14 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import *
 
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    user=request.session.get('user')
+    return render(request,'index.html',{'user':user})
 
 def login(request):
+    if request.method=='POST':
+        unm=request.POST['username']
+        pas=request.POST['password']
+
+        user=usersignup.objects.filter(username=unm,password=pas)
+        if user:
+            print("Login Successfully!")
+            request.session['user']=unm
+            return redirect('/')
+        else:
+            print("Error!Login faild....")
     return render(request,'login.html')
 
 def signup(request):
+    if request.method=='POST':
+        newuser=signupForm(request.POST)
+        if newuser.is_valid():
+            newuser.save()
+            print("Signup Successfully!")
+            return redirect('login')
+        else:
+            print(newuser.errors)
     return render(request,'signup.html')
 
 def notes(request):
